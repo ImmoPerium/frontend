704c94cfca1a7a8db0c5eecca27fbf707c203a09
Ilja2307 <48624103+Ilja2307@users.noreply.github.com>
2020-10-27 21:41:57 +0100

    Pre-configurations so we can start coding - project start

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # frontend
-A React.js frontend for an ImmoScout24 clone made during Leuphana University module IT-Projektmanagement 2020/2021.
+A React.js frontend for an ImmoScout24 clone made during Leuphana University module IT-Projectmanagement 2020/2021.
---
+++ immoperiumfrontend/src/App.js
@@ -0,0 +1,19 @@
+import './tailwind.output.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div class="h-24 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500" >IMMOPERIUM</div><br></br>
+        <div>LOGIN</div>
+        <p class="text-purple-700 text-opacity-100">The quick brown fox ...</p>
+        <p class="text-purple-700 text-opacity-75">The quick brown fox ...</p>
+        <p class="text-purple-700 text-opacity-50">The quick brown fox ...</p>
+        <p class="text-purple-700 text-opacity-25">The quick brown fox ...</p>
+        <p class="text-purple-700 text-opacity-0">The quick brown fox ...</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
