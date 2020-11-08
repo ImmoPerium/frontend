6ec72e91f93a00e3b17fa323c6348f97c76e109c
Sascha Majewsky <sascha.majewsky@pm.me>
2020-11-08 12:19:46 +0100

    Added example react classes for view and component

--- immoperiumfrontend/src/App.js
+++ immoperiumfrontend/src/App.js
@@ -2,6 +2,7 @@ import './tailwind.output.css';
 import { Route, Switch, Redirect } from 'react-router-dom';
 /* import Register from './views/Register.js';
 import Bills from './Components/Bills.js'; */
+import Logout from './views/Logout.js'
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       {/* <NavBar token={token} setToken={setToken} /> */}
       <Switch>
       {/* <Route exact path='/register' render={(...props) => <Register {...props} />} /> */}
+      <Route exact path='/logout' render={(...props) => <Logout {...props} />} />
       {/* <Route exact path='/'render={(...props) => token && user ? <Redirect to='/dashboard' /> : <Redirect to='/login' /> } */}
       </Switch>
     </div>
---
+++ immoperiumfrontend/src/components/ExampleComponent.js
@@ -0,0 +1,15 @@
+  import React from "react";
+
+  /* A template for a simple React component */
+class Example extends React.Component {
+
+/* React Class functions here */
+
+
+/* Rendered JSX here */
+  render() {
+    return "Example";
+  }
+}
+
+export default Example;
