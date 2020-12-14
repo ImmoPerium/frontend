5a69f5c608923f5e3389287024e76831e8bb9e26
Sascha Majewsky <sascha.majewsky@pm.me>
2020-12-14 11:49:41 +0100

    Refactored styling of new offering view

--- immoperiumfrontend/src/views/NewOffer.js
+++ immoperiumfrontend/src/views/NewOffer.js
@@ -12,7 +12,7 @@ class NewOffer extends React.Component {
 
   render() {
     return (
-      <div className="bg-gray-200 pt-8 pb-12">
+      <div className="bg-gray-200 pt-8 pb-12 min-h-screen">
         <div className="mt-4">
           <div className="md:grid md:grid-cols-3 md:gap-6 ">
             <div className="md:col-span-1">
@@ -49,7 +49,7 @@ class NewOffer extends React.Component {
                             type="text"
                             name="company_website"
                             id="company_website"
-                            className="outline-none focus:outline-none border-2 border-orange-200 focus:border-orange-500 flex-1 block w-full rounded-none rounded-r-md sm:text-"
+                            className="outline-none focus:outline-none border-2 border-gray-200 focus:border-gray-500 flex-1 block w-full rounded-none rounded-r-md sm:text-"
                             placeholder=" Deine Immobilie in einem Satz"
                           />
                         </div>
@@ -67,7 +67,7 @@ class NewOffer extends React.Component {
                           id="about"
                           name="about"
                           rows={3}
-                          className="shadow-sm outline-none focus:outline-none border-2 border-orange-200 focus:border-orange-500 mt-1 block w-full sm:text-sm rounded-md"
+                          className="shadow-sm outline-none focus:outline-none border-2 border-gray-200 focus:border-gray-500 mt-1 block w-full sm:text-sm rounded-md"
                           placeholder=" Was macht die Immobilie besonders? Wie könnte man alle Vorteile kurz zusammenfassen?"
                           defaultValue={""}
                         />
@@ -193,7 +193,7 @@ class NewOffer extends React.Component {
                           name="first_name"
                           id="first_name"
                           autoComplete="given-name"
-                          className="mt-1 outline-none focus:outline-none border-2 border-orange-200 focus:border-orange-500 block w-full py-1 shadow-sm sm:text-sm rounded-md"
+                          className="mt-1 outline-none focus:outline-none border-2 border-gray-200 focus:border-gray-500 block w-full py-1 shadow-sm sm:text-sm rounded-md"
                         />
                       </div>
                       <div className="col-span-6 sm:col-span-3">
@@ -208,7 +208,7 @@ class NewOffer extends React.Component {
                           name="last_name"
                           id="last_name"
                           autoComplete="family-name"
-                          className="mt-1 outline-none focus:outline-none border-2 border-orange-200 focus:border-orange-500 block w-16 py-1 shadow-sm sm:text-sm rounded-md"
+                          className="mt-1 outline-none focus:outline-none border-2 border-gray-200 focus:border-gray-500 block w-16 py-1 shadow-sm sm:text-sm rounded-md"
                         />
                       </div>
                       <div className="col-span-6 sm:col-span-6 lg:col-span-2">
@@ -222,7 +222,7 @@ class NewOffer extends React.Component {
                           type="text"
                           name="city"
                           id="city"
-                          className="mt-1 outline-none focus:outline-none border-2 border-orange-200 focus:border-orange-500 block w-full py-1 shadow-sm sm:text-sm rounded-md"
+                          className="mt-1 outline-none focus:outline-none border-2 border-gray-200 focus:border-gray-500 block w-full py-1 shadow-sm sm:text-sm rounded-md"
                         />
                       </div>
                       <div className="col-span-6 sm:col-span-3 lg:col-span-2">
@@ -236,7 +236,7 @@ class NewOffer extends React.Component {
                           type="text"
                           name="state"
                           id="state"
-                          className="mt-1 outline-none focus:outline-none border-2 border-orange-200 focus:border-orange-500 block w-full py-1 shadow-sm sm:text-sm rounded-md"
+                          className="mt-1 outline-none focus:outline-none border-2 border-gray-200 focus:border-gray-500 block w-full py-1 shadow-sm sm:text-sm rounded-md"
                         />
                       </div>
                       <div className="col-span-6 sm:col-span-3 lg:col-span-2">
@@ -251,7 +251,7 @@ class NewOffer extends React.Component {
                           name="postal_code"
                           id="postal_code"
                           autoComplete="postal-code"
-                          className="mt-1 outline-none focus:outline-none border-2 border-orange-200 focus:border-orange-500 block w-20 py-1 shadow-sm sm:text-sm rounded-md"
+                          className="mt-1 outline-none focus:outline-none border-2 border-gray-200 focus:border-gray-500 block w-20 py-1 shadow-sm sm:text-sm rounded-md"
                         />
                       </div>
                       <div className="col-span-6 sm:col-span-3">
@@ -283,162 +283,6 @@ class NewOffer extends React.Component {
         <div className="hidden sm:block" aria-hidden="true">
           <div className="py-5">
             <div className="border-t border-gray-200" />
-          </div>
-        </div>
-        <div className="mt-10 sm:mt-0">
-          <div className="md:grid md:grid-cols-3 md:gap-6">
-            <div className="md:col-span-1">
-              <div className="px-4 sm:px-0 ml-8">
-                <h3 className="text-lg font-medium leading-6 text-gray-900">
-                  Notifications
-                </h3>
-                <p className="mt-1 text-sm text-gray-600">
-                  Decide which communications you'd like to receive and how.
-                </p>
-              </div>
-            </div>
-            <div className="mt-5 md:mt-0 md:col-span-2">
-              <form action="#" method="POST">
-                <div className="shadow overflow-hidden sm:rounded-md">
-                  <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
-                    <fieldset>
-                      <legend className="text-base font-medium text-gray-900">
-                        By Email
-                      </legend>
-                      <div className="mt-4 space-y-4">
-                        <div className="flex items-start">
-                          <div className="flex items-center h-5">
-                            <input
-                              id="comments"
-                              name="comments"
-                              type="checkbox"
-                              className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
-                            />
-                          </div>
-                          <div className="ml-3 text-sm">
-                            <label
-                              htmlFor="comments"
-                              className="font-medium text-gray-700"
-                            >
-                              Comments
-                            </label>
-                            <p className="text-gray-500">
-                              Get notified when someones posts a comment on a
-                              posting.
-                            </p>
-                          </div>
-                        </div>
-                        <div className="flex items-start">
-                          <div className="flex items-center h-5">
-                            <input
-                              id="candidates"
-                              name="candidates"
-                              type="checkbox"
-                              className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
-                            />
-                          </div>
-                          <div className="ml-3 text-sm">
-                            <label
-                              htmlFor="candidates"
-                              className="font-medium text-gray-700"
-                            >
-                              Candidates
-                            </label>
-                            <p className="text-gray-500">
-                              Get notified when a candidate applies for a job.
-                            </p>
-                          </div>
-                        </div>
-                        <div className="flex items-start">
-                          <div className="flex items-center h-5">
-                            <input
-                              id="offers"
-                              name="offers"
-                              type="checkbox"
-                              className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
-                            />
-                          </div>
-                          <div className="ml-3 text-sm">
-                            <label
-                              htmlFor="offers"
-                              className="font-medium text-gray-700"
-                            >
-                              Offers
-                            </label>
-                            <p className="text-gray-500">
-                              Get notified when a candidate accepts or rejects
-                              an offer.
-                            </p>
-                          </div>
-                        </div>
-                      </div>
-                    </fieldset>
-                    <fieldset>
-                      <div>
-                        <legend className="text-base font-medium text-gray-900">
-                          Push Notifications
-                        </legend>
-                        <p className="text-sm text-gray-500">
-                          These are delivered via SMS to your mobile phone.
-                        </p>
-                      </div>
-                      <div className="mt-4 space-y-4">
-                        <div className="flex items-center">
-                          <input
-                            id="push_everything"
-                            name="push_notifications"
-                            type="radio"
-                            className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
-                          />
-                          <label
-                            htmlFor="push_everything"
-                            className="ml-3 block text-sm font-medium text-gray-700"
-                          >
-                            Everything
-                          </label>
-                        </div>
-                        <div className="flex items-center">
-                          <input
-                            id="push_email"
-                            name="push_notifications"
-                            type="radio"
-                            className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
-                          />
-                          <label
-                            htmlFor="push_email"
-                            className="ml-3 block text-sm font-medium text-gray-700"
-                          >
-                            Same as email
-                          </label>
-                        </div>
-                        <div className="flex items-center">
-                          <input
-                            id="push_nothing"
-                            name="push_notifications"
-                            type="radio"
-                            className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
-                          />
-                          <label
-                            htmlFor="push_nothing"
-                            className="ml-3 block text-sm font-medium text-gray-700"
-                          >
-                            No push notifications
-                          </label>
-                        </div>
-                      </div>
-                    </fieldset>
-                  </div>
-                  <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                    <button
-                      type="submit"
-                      className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                      Save
-                    </button>
-                  </div>
-                </div>
-              </form>
-            </div>
           </div>
         </div>
       </div>
